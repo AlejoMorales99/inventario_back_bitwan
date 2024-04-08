bc841582d9872cf33d2b9fc8e96dc523e50e5cf3
AlejoMorales99 <leagueofleguends122@gmail.com>
2024-04-08 11:17:39 -0500

    variablesBack

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: '.env' });
 
 const app = require('./app'); // Importo mi aplicación Express desde el archivo app.js
 
-const IP = process.env.IP_HOST;
+const IP = process.env.IP_HOST_PRUEBAS;
 
 const PORT = process.env.PORT_APP || 4003; // Establezco el puerto en el que se ejecutará el servidor, tomando el valor de la variable de entorno PORT_APP si está definida, de lo contrario, uso el puerto 4003
 
